79b4aab7b21edb26fb70469d5dd267da7cfad42b
Ashish Verma <ashishverma510207@gmail.com>
2023-12-08 00:57:23 +0530

    created components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+const App = () => {
+  return (
+    <div>
+      <Home></Home>
+      <Login></Login>
+      <Signup></Signup>
+    </div>
+  )
+}
+
+export default App
