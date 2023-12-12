af09b9786156ed6bfba1669966fe4546b3628091
Ashish Verma <ashishverma510207@gmail.com>
2023-12-12 15:38:26 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,29 @@ import './App.css'
 import Home from './components/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
+import Contact from './components/Contact'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <Home></Home>
-      <Login></Login>
-      <Signup></Signup>
+      <BrowserRouter>
+
+        {/* <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link> */}
+
+        <Navbar />
+
+        <Routes>
+          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
