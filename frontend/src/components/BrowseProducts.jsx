f0959239d7e6da7e6417898e526a7386847c6a7d
Ashish Verma <ashishverma510207@gmail.com>
2023-12-19 15:30:16 +0530

    Update product list and search functionality in BrowseProducts component

--- frontend/src/components/BrowseProducts.jsx
+++ frontend/src/components/BrowseProducts.jsx
@@ -1,14 +1,17 @@
 import React, { useState , useRef} from 'react'
 import laptopData from './dummyData'
+// import laptops from './dummyData';
 
 const BrowseProducts = () => {
 
     const [productList, setProductList] = useState(laptopData);
+    // const [productList, setProductList] = useState(laptops);
 
     const searchRef = useRef(null);
     const searchProduct = ()=>{
         const filteredData = laptopData.filter((laptop)=>{
             return laptop.brand.toLowerCase().includes(searchRef.current.value.toLowerCase());
+            // return laptop.name.toLowerCase().includes(searchRef.current.value.toLowerCase());
         })
         setProductList(filteredData);
     }
@@ -59,11 +62,24 @@ const BrowseProducts = () => {
                             <h4 className='fw-bold'>₹{laptop.price} </h4>
                         </div>
                     </div>
+
+                    {/* <div className='card'>
+                        <img src={laptop.img_link} alt="" />
+                        <div className='card-body'>
+                            <h5>{laptop.name}</h5>
+                            <h4>{laptop.processor}</h4>
+                            <p>{laptop.no_of_ratings} stars from reviews</p>
+                            <button className='btn btn-primary btn-sm float-end'>Buy now</button>
+                            <h4 className='fw-bold'>₹{laptop.price} </h4>
+                        </div>
+                    </div> */}
+
             </div>)
             })}
             </div>
                 </div>
             </div>
+
             {/* <div className='row gy-4'>
             {productList.map((laptop)=>{
                 return (<div className='col-md-3'>
--- frontend/src/components/dummyData.js
+++ frontend/src/components/dummyData.js
@@ -8,6 +8,19 @@ const laptopData = [
         reviews: 8373
     },
     {
+        image: "https://rukminim1.flixcart.com/image/312/312/xif0q/computer/y/4/b/-original-imagmmntzeunqhyh.jpeg?q=70",
+        brand: "Lenovo Intel Core i3 11th Gen",
+        price: 38290,
+        processor: "Intel Core i3 Processor (11th Gen)",
+        ram: "8 GB DDR4 RAM",
+        os: "64 bit Windows 11 Operating System",
+        storage: "512 GB SSD",
+        display: 14,
+        rating: "3.9",
+        rating: "37.0",
+        reviews: "1.0"
+      },
+    {
         brand: 'HP',
         model: 'Victus',
         image: 'https://i1.wp.com/laptopmedia.com/wp-content/uploads/2021/09/61WGh6U2fXL._AC_SL1500_.jpg?fit=1352%2C1152&ssl=1',
