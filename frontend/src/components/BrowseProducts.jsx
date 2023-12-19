f165a465cdb789e2a929ebe19bf7feda199f78ec
Ashish Verma <ashishverma510207@gmail.com>
2023-12-19 14:13:27 +0530

    Added search and filter functionality to the laptop e-commerce website.

--- frontend/src/components/BrowseProducts.jsx
+++ frontend/src/components/BrowseProducts.jsx
@@ -1,25 +1,85 @@
-import React, { useState } from 'react'
+import React, { useState , useRef} from 'react'
 import laptopData from './dummyData'
 
 const BrowseProducts = () => {
 
     const [productList, setProductList] = useState(laptopData);
 
+    const searchRef = useRef(null);
+    const searchProduct = ()=>{
+        const filteredData = laptopData.filter((laptop)=>{
+            return laptop.brand.toLowerCase().includes(searchRef.current.value.toLowerCase());
+        })
+        setProductList(filteredData);
+    }
+
+    const priceRef = useRef(null);
+    const filterPrice = (e)=>{
+        const filteredData = laptopData.filter((laptop)=>{
+            return laptop.price<=parseInt(e.target.value);
+        })
+        setProductList(filteredData);
+    }
+
+
+
+ //searchRef.current.value = e.target.value
   return (
     <div>
-        <div className='container'>
-            <div className='row gy-4'>
+        <header className='bg-dark text-white'>
+            <div className='container py-5'>
+                <h1 className='text-center'>Search Laptop</h1>
+                <div className='input-group'>
+                    <input type='text' className='form-control' ref={searchRef}/>
+                    <button className='btn btn-primary' onClick={searchProduct}>Search</button>
+                </div>
+            </div>
+        </header>
+        <div className='container-fluid'>
+            <div className="row">
+                <div className='col-md-3'>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <label>Max Price</label>
+                            <input type='range' min={10000} max={200000} step={5000} className='form-range' onChange={filterPrice} />
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-9'>
+                <div className='row gy-4'>
             {productList.map((laptop)=>{
                 return (<div className='col-md-3'>
                     <div className='card'>
                         <img src={laptop.image} alt="" />
                         <div className='card-body'>
+                            <h5>{laptop.brand}</h5>
                             <h4>{laptop.model}</h4>
+                            <p>{laptop.rating} stars from reviews</p>
+                            <button className='btn btn-primary btn-sm float-end'>Buy now</button>
+                            <h4 className='fw-bold'>₹{laptop.price} </h4>
                         </div>
                     </div>
             </div>)
             })}
             </div>
+                </div>
+            </div>
+            {/* <div className='row gy-4'>
+            {productList.map((laptop)=>{
+                return (<div className='col-md-3'>
+                    <div className='card'>
+                        <img src={laptop.image} alt="" />
+                        <div className='card-body'>
+                            <h5>{laptop.brand}</h5>
+                            <h4>{laptop.model}</h4>
+                            <p>{laptop.rating} stars from reviews</p>
+                            <button className='btn btn-primary btn-sm float-end'>Buy now</button>
+                            <h4 className='fw-bold'>₹{laptop.price} </h4>
+                        </div>
+                    </div>
+            </div>)
+            })}
+            </div> */}
         </div>
     </div>
   )
--- frontend/src/components/dummyData.js
+++ frontend/src/components/dummyData.js
@@ -8,12 +8,12 @@ const laptopData = [
         reviews: 8373
     },
     {
-        brand: 'Lenevo',
-        model: 'Ideapad 330',
-        image: 'https://th.bing.com/th/id/OIP.BdkLcplp-nVpY0Wbz4MaxQHaGk?w=198&h=180&c=7&r=0&o=5&pid=1.7',
-        price: 46900,
-        rating: 4.5,
-        reviews: 8373
+        brand: 'HP',
+        model: 'Victus',
+        image: 'https://i1.wp.com/laptopmedia.com/wp-content/uploads/2021/09/61WGh6U2fXL._AC_SL1500_.jpg?fit=1352%2C1152&ssl=1',
+        price: 66900,
+        rating: 4.9,
+        reviews: 83732323
     },
     {
         brand: 'Lenevo',
