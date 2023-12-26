48c5118c5537a49c5baf77049ce0d06a13088a65
Ashish Verma <ashishverma510207@gmail.com>
2023-12-26 18:43:38 +0530

    signup form updated

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useFormik} from 'formik'
 import * as Yup from 'yup';
 
@@ -6,15 +6,20 @@ const SignupSchema = Yup.object().shape({
   name: Yup.string().required('Name is required').min(4,'Name is too short'),
   email: Yup.string().email('Invalid email').required('Required'),
   password:Yup.string().required('Password is required').min(8,'Password is too short')
+  .matches(/[a-zA-Z]\d/, 'password must include uppercase and lowercase letter'),
+  confirm:Yup.string().oneOf([Yup.ref('password'),null], 'Password must match').required('confirm password is required')
 });
 
 const Signup = () => {
   
+  const [passwordHidden, setPasswordHidden] = useState('password')
+
   const signupForm = useFormik({
     initialValues:{
       name:'',
       email:'',
-      password:''
+      password:'',
+      confirm:''
     },
     onSubmit:(values)=>{
       console.log(values);
@@ -31,7 +36,7 @@ const Signup = () => {
       <div data-mdb-input-init class="form-outline">
         <input type="text" class="form-control" id="name" onChange={signupForm.handleChange} value={signupForm.values.name} /> 
         <label class="form-label" for="form3Example1">Name</label>
-        <span className='ms-4 fs-6 text-danger'>{signupForm.errors.name}</span>
+        <span className='ms-4 fs-6 text-danger'>{signupForm.touched.name && signupForm.errors.name}</span>
       </div>
     </div>
     {/* <div class="col">
@@ -45,13 +50,21 @@ const Signup = () => {
   <div data-mdb-input-init class="form-outline mb-4">
     <input type="email" class="form-control" id='email' onChange={signupForm.handleChange} value={signupForm.values.email}/>
     <label class="form-label" for="form3Example3">Email address</label>
+    <span className='ms-4 fs-6 text-danger'>{signupForm.touched.email && signupForm.errors.email}</span>
   </div>
 
   <div data-mdb-input-init class="form-outline mb-4">
     <input type="password" id="password" onChange={signupForm.handleChange} value={signupForm.values.password} class="form-control" />
     <label class="form-label" for="form3Example4">Password</label>
+    <span className='ms-4 fs-6 text-danger'>{signupForm.touched.password &&  signupForm.errors.password}</span>
   </div>
 
+  <div data-mdb-input-init class="form-outline mb-4">
+    <input type={passwordHidden?'password':'text'} id="confirm" onChange={signupForm.handleChange} value={signupForm.values.confirm} class="form-control" />
+    <label class="form-label" for="form3Example4">Confirm Password</label>
+    <span className='ms-4 fs-6 text-danger'>{signupForm.touched.confirm && signupForm.errors.confirm}</span>
+    <button type='button' onClick={()=>{setPasswordHidden(!passwordHidden)}}>{passwordHidden?'Show':'Hide'}</button>
+  </div>
   {/* <div class="form-check d-flex justify-content-center mb-4">
     <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
     <label class="form-check-label" for="form2Example33">
