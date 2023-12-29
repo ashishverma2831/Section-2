3fae18ada1225b88c79b76f40c76529e879a99b9
Ashish Verma <ashishverma510207@gmail.com>
2023-12-29 13:49:03 +0530

    user router

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -4,7 +4,18 @@ const router = express.Router();
 router.get('/add',(req,res)=>{
     res.send('add response from user router')
 })
-
+router.get('/getall',(req,res)=>{
+    res.send('get all response from user router')
+})
+router.get('/getbyid',(req,res)=>{
+    res.send('get by id response from user router')
+})
+router.get('/update',(req,res)=>{
+    res.send('update response from user router')
+})
+router.get('/delete',(req,res)=>{
+    res.send('delete response from user router')
+})
 //getAll
 //getbyid
 //update
